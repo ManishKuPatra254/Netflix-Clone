3f461b56b178d5d84c224c3233827c21dd2e2a31
ManishKuPatra254 <manishpatra254@gmail.com>
2023-07-09 14:24:43 +0530

    added video and some styles

--- src/MainHomeContents/MainHomeContents.js
+++ src/MainHomeContents/MainHomeContents.js
@@ -1,9 +1,23 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import styles from './MainHomeContents.module.css';
+import image2 from '../Image Section/netflix-logo-png-2562.png'
+import videoTitle from '../Image Section/Cropped_Video.mp4';
 
 export function MainHomeContents() {
     return (
         <Fragment>
-
+            <div className={styles.main_home_nav}>
+                <video autoPlay muted loop>
+                    <source src={videoTitle} type='video/mp4' />
+                </video>
+                <div className={styles.main_home_sec} >
+                    <div className={styles.heading_logo}>
+                        <img src={image2} alt="" />
+                    </div>
+                    <div className={styles.list_nav}>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
