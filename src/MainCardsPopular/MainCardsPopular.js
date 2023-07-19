5414d3f598bfd72c82a38606ee0d3939e828327e
ManishKuPatra254 <manishpatra254@gmail.com>
2023-07-19 13:31:25 +0530

    added a new popular

--- src/MainCardsPopular/MainCardsPopular.js
+++ src/MainCardsPopular/MainCardsPopular.js
@@ -1,10 +1,16 @@
 import { Fragment, useEffect, useState } from 'react';
 import styles from './MainCardsPopular.module.css';
 import axios from 'axios';
+import { Button } from '@mui/material';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import CloseIcon from '@mui/icons-material/Close';
 
 export function MainCardsPopular() {
 
     const [trendingOnNetflix, setTrendingOnNetflix] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [caption, setCaption] = useState('');
+    const [overview, setOverview] = useState('');
 
     async function fetchTrendData() {
         try {
@@ -21,13 +27,28 @@ export function MainCardsPopular() {
         fetchTrendData();
     }, []);
 
+    function handleClose() {
+        setSelectedMovie(null);
+        setCaption('');
+    };
+
+    function handleImageClick(posterPath, title, overview) {
+        setSelectedMovie(posterPath);
+        setCaption(title);
+        setOverview(overview)
+    }
+
     return (
         <Fragment>
             <div className={styles.main_movie_contained}>
                 <h4>Popular on Netflix</h4>
                 <div className={styles.main_movie}>
                     {trendingOnNetflix.map((trending) => (
-                        <div className={styles.sub_movies_cards} key={trending.id}>
+                        <div
+                            className={`${styles.sub_movies_cards} ${selectedMovie === trending.poster_path ? styles.selected : ''}`}
+                            key={trending.id}
+                            onClick={() => handleImageClick(trending.poster_path, trending.title, trending.overview)}
+                        >
                             <img
                                 className={styles.movie_image_card}
                                 src={`https://image.tmdb.org/t/p/w200${trending.poster_path}`}
@@ -36,6 +57,50 @@ export function MainCardsPopular() {
                         </div>
                     ))}
                 </div>
+                {selectedMovie && (
+                    <div className={styles.selected_image_container}>
+                        <img
+                            className={styles.selected_image}
+                            src={`https://image.tmdb.org/t/p/w200${selectedMovie}`}
+                            alt="Selected Movie"
+                        />
+
+                        <div className={styles.caption_new}>{caption}</div>
+                        <div className={styles.overview_new}>{overview}</div>
+                        <div className={styles.btn_container}>
+                            <Button
+                                sx={{
+                                    display: 'flex',
+                                    backgroundColor: 'red',
+                                    "&:hover": { backgroundColor: '#c00b14' },
+                                    padding: '8px 35px',
+                                    fontSize: '16px',
+                                    marginTop: '10px'
+                                }}
+                                variant="contained"
+                            >
+                                <PlayArrowIcon /> PLAY
+                            </Button>
+
+                            <Button
+                                onClick={handleClose}
+                                sx={{
+                                    display: 'flex',
+                                    backgroundColor: 'rgb(39, 39, 39)',
+                                    "&:hover": { backgroundColor: 'rgb(70, 70, 70)', color: 'white' },
+                                    color: 'white',
+                                    padding: '8px 35px',
+                                    fontSize: '16px',
+                                    marginTop: '10px',
+                                }}
+                                variant="contained"
+                            >
+                                <CloseIcon /> CLOSE
+                            </Button>
+
+                        </div>
+                    </div>
+                )}
             </div>
         </Fragment>
     )
